eaed575cd78614c8397615adf02f75960e3a6b7e
Emily Pasqualotto <50452427+memelee0@users.noreply.github.com>
2021-07-21 14:51:22 -0300

    Add files via upload

---
+++ js/candidatos.js
@@ -0,0 +1,75 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '24999',
+                nome: 'SHACO',
+                partido: 'Partido Democrático Liga das Lendas - PDL',
+                fotos: [
+                    {url:'shaco.jpg', legenda: 'Vereador'}
+                ]
+            },
+
+            {
+                numero: '69123',
+                nome: 'ANNIE',
+                partido:'Partido das Bonitas - PB',
+                fotos: [
+                    {url:'annie.jpg', legenda: 'Vereador'}
+                ]
+            },
+
+            {
+                numero: '24123',
+                nome: 'GAREN',
+                partido: 'Partido Democrático Liga das Lendas - PDL',
+                fotos: [
+                    {url:'garen.jpg', legenda: 'Vereador'}
+                ]
+            },
+
+            {
+                numero: '24444',
+                nome: 'TEEMO',
+                partido: 'Partido Democrático Liga das Lendas - PDL',
+                fotos: [
+                    {url:'teemo.jpg', legenda: 'Vereador'}
+                ]
+            },
+
+
+           
+
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero:'24',
+                nome:'MORGANA',
+                partido:'Partido Democrático Liga das Lendas - PDL',
+                vice: 'EVELYYN',
+                fotos:[
+                    {url:'morgana.jpg', legenda: 'Prefeito'},
+                    {url:'evelin.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+
+            {
+                numero:'69',
+                nome:'AHRI',
+                partido:'Partido das Bonitas - PB',
+                vice: 'SERAPHINE',
+                fotos:[
+                    {url:'ahri.jpg', legenda: 'Prefeito'},
+                    {url:'seraphine.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            
+        ]
+    }
+];
